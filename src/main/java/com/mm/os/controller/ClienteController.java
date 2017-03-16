a05a1d060a3167acf8014c7361c9b509cb87d8b7
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-03-16 03:23:22 -0300

    Client repository

--- src/main/java/com/mm/os/controller/ClienteController.java
+++ src/main/java/com/mm/os/controller/ClienteController.java
@@ -1,14 +1,22 @@
 package com.mm.os.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.mm.os.repository.Clientes;
 
 @Controller
 public class ClienteController {
 	
-	@GetMapping("/")
-	public String hello(){
-		return "Hello";
+	@Autowired
+	private Clientes clientes;
+	
+	@GetMapping("/clientes")
+	public ModelAndView hello(){
+		ModelAndView modelAndView = new ModelAndView("Hello");
+		modelAndView.addObject("clientes", clientes.findAll());
+		return modelAndView;
 	}
 
 }
